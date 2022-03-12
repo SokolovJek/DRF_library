e9778d8d0b727627f89502cca7c781ddf5c871a0
SokolovJek <sokolovgek@mail.ru>
2022-03-12 20:47:15 +0300

    работа с претставлениями

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,19 +1,24 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
 from rest_framework import serializers
 from .models import ProjectModel, TodoModel
-from authors.serializers import AuthorsModelSerializers
 
 
-class ProjectModelSerializers(serializers.ModelSerializer):
-    # users = serializers.StringRelatedField(many=True)
+class ProjectModelSerializers(serializers.HyperlinkedModelSerializer):
+    set_todo = serializers.SerializerMethodField()
 
     class Meta:
         model = ProjectModel
-        exclude = ['uid']
+        fields = '__all__'
+
+    def get_set_todo(self, obj):
+        a = []
+        for i in TodoModel.objects.filter(project=obj.pk):
+           a.append(str(i))
+        return a
 
 
 class TodoModelSerializers(serializers.HyperlinkedModelSerializer):
-
+    project = serializers.StringRelatedField()
+    users = serializers.StringRelatedField()
 
     class Meta:
         model = TodoModel
